b863702684a4c29e4e9ecbf6f8c516a3744da3ca
nandaaliputra <134515326+nandaaliputra@users.noreply.github.com>
2023-06-12 13:51:23 +0700

    membuat capstone

--- capstone.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.set_page_config(layout='wide')
-
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/1g9Qd6nnuRW9msVXQg3O1v91cx_7UWTP-vrcptdZgIP8/edit?hl=id#gid=658586720')
-
-df
